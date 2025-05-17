eac2bbfe7b547778928c83bfb4585d162e7f48da
Hcode7 <ratitamine3@gmail.com>
2025-05-18 00:46:46 +0100

    deploy project

--- system/settings.py
+++ system/settings.py
@@ -26,8 +26,15 @@ SECRET_KEY = 'django-insecure-2ig%kh&&sz9e*=6tx2@6$piugr*r))&5x5%4u*k0yyw1*5j3m&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost' , '127.0.0.1', 'gymtracker.up.railway.app']
 
+
+CSRF_COOKIE_SECURE = True
+CSRF_TRUSTED_ORIGINS = ['https://gymtracker.up.railway.app'] 
+
+ACCOUNT_DEFAULT_HTTP_PROTOCOL = 'https'
+
+SITE_ID = 1
 
 # Application definition
 
